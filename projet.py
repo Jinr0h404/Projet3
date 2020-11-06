d584c9407ba2c8b0104084a8cf0d39c7eb0be841
ewen jeannenot <ewen.jeannenot@normandie.cci.fr>
2020-11-06 12:29:00 +0100

    create class character

--- projet.py
+++ projet.py
@@ -13,6 +13,31 @@ class Map:
 		with open(area, "r") as map_file:
 			zone = map_file.read()
 			return zone
-			
+
+
+class Character:
+	""" Character class allow to create obj character with position attribute
+	and move method"""
+	def __init__(self, name, first_position):
+		self.name = name
+		self.position = first_position
+		self.item = {"aiguille":"False", "ether":"False","tube":"False"} 
+		# move methode check if sprite is with an item, if yes, change
+		# value of dict "item" to true with item key
+		# for the last move if all item value != True, it's lost
+
+	def move_up(self):
+		self.position = self.position[1] += 1 #if position define by list of 2 item abs and ordo
+
+	def move_down(self, input_key):
+		pass
+
+	def move_left(self, input_key):
+		pass
+
+	def move_right(self, input_key):
+		pass
+
+
 test_map = Map()
 print(test_map.area("laby.txt"))
