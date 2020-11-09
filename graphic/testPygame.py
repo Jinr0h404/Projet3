51be7a1d2a2192bb212993748f718fc5de5b04b1
ewen jeannenot <ewen.jeannenot@normandie.cci.fr>
2020-11-09 12:01:59 +0100

    test pygame add floor

--- graphic/testPygame.py
+++ graphic/testPygame.py
@@ -12,9 +12,12 @@ picture_macgy = pygame.image.load("../data/MacGyver.png").convert()
 picture_floor = pygame.image.load("../data/floor15.png").convert()
 picture_wall = pygame.image.load("../data/wall15.png").convert()
 
+list_wall = [(10, 10),(30, 30), (50,50)]
+
 continuer = True
 
 while continuer:
+    ecran.blit(picture_floor, list_wall[2])
     ecran.blit(picture_macgy, (0, 0)) # le tuple 0,0 donne la position de départ de l'image
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
