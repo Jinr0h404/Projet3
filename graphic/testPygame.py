e269f7b7466dbf56f833f39475da69bdf28ff3f2
ewen <ewen.jeannenot@normandie.cci.fr>
2020-11-10 16:41:12 +0100

    test for floor map in pygame

--- graphic/testPygame.py
+++ graphic/testPygame.py
@@ -2,27 +2,37 @@ import pygame
 
 pygame.init()
 
-ecran = pygame.display.set_mode((500, 300))
+ecran = pygame.display.set_mode((675, 675))
 pygame.display.set_caption("Help MacGyver")
-picture_menu = pygame.image.load("../data/projet_3.png").convert()
+picture_menu = pygame.image.load("../data/projet_3.png").convert_alpha()
 pygame.display.set_icon(picture_menu)
 
 
-picture_macgy = pygame.image.load("../data/MacGyver.png").convert()
-picture_floor = pygame.image.load("../data/floor15.png").convert()
-picture_wall = pygame.image.load("../data/wall15.png").convert()
+noir = (10, 10, 10)
 
-list_wall = [(10, 10),(30, 30), (50,50)]
+picture_macgy = pygame.image.load("../data/MacGyver.png").convert_alpha() # cherche et tranforme l'image en surface puis la converti
+picture_floor = pygame.image.load("../data/floor15.png").convert_alpha()
+picture_wall = pygame.image.load("../data/wall15.png").convert_alpha()
+
+rect_macgy = picture_macgy.get_rect()
+rect_floor = picture_floor.get_rect()
+rect_wall = picture_wall.get_rect()
+
+
+
+list_wall = [(10, 10),(30, 30), (50,50), (90,90), (120,120), (150, 150)]
 
 continuer = True
 
-while continuer:
-
-    ecran.blit(picture_floor, list_wall[2])
-    ecran.blit(picture_macgy, (0, 0)) # le tuple 0,0 donne la position de départ de l'image
+while continuer: 
+    ecran.fill(noir) # definit une couleur de fond pour l'ecran
+    for element in list_wall:
+        ecran.blit(picture_floor, (element))
+    ecran.blit(picture_floor, list_wall[2]) # blit copie l'image_surface en parametre sur la surface de l'ecran
+    ecran.blit(picture_macgy, (0, 0, 20, 20)) # le tuple 0,0 donne la position de départ de l'image
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.QUIT:
             continuer = False
-    pygame.display.flip()
+    pygame.display.flip() # dit à pygame d'afficher la surface du jeu
 
 pygame.quit()
--- projet.py
+++ projet.py
@@ -1,3 +1,5 @@
+import pygame
+
 """ this project is a game. The goal is to move the character in a 15 / 15 
     square map and find the exit """
 
@@ -72,10 +74,70 @@ print(test_values)
 
 
 list_possible_position = []
-for i in list_map:
+list_wall_position = []
+list_floor_position = []
+for i in list_map:       # pour chaque element de ma liste de dictionnaire
     dico = i
-    for i in dico.values():
+    print(dico)
+    for i in dico.values(): # je recupere la valeur de mon dico
+        print(i)
         if i == "O":
-            list_possible_position.append(dico)
+            list_possible_position.append(dico) # si valeur == O alors j'ajoute le dico a la liste possible
+            for key in dico.keys():
+                list_floor_position.append(key)   # et je met la clef du dico donc sa position dans la liste des positions de sol
+        else:
+            for key in dico.keys():
+                list_wall_position.append(key)  # et je mets le reste des clefs donc les positions de murs, dans la liste des positions de mur
 print(list_possible_position)
+print(list_wall_position)
+print(list_floor_position)
 
+
+"""ici je vais convertir mes listes de position sol et mur en coordonnée de pixel"""
+
+
+
+
+
+"""ici je teste la partie pygame avant de tout separer en module"""
+
+pygame.init()
+
+ecran = pygame.display.set_mode((675, 675))
+pygame.display.set_caption("Help MacGyver")
+picture_menu = pygame.image.load("data/projet_3.png").convert_alpha()
+pygame.display.set_icon(picture_menu)
+
+
+noir = (10, 10, 10)
+
+picture_macgy = pygame.image.load("data/MacGyver.png").convert_alpha() # cherche et tranforme l'image en surface puis la converti
+picture_floor = pygame.image.load("data/floor15.png").convert_alpha()
+picture_wall = pygame.image.load("data/wall15.png").convert_alpha()
+
+rect_macgy = picture_macgy.get_rect()
+rect_floor = picture_floor.get_rect()
+rect_wall = picture_wall.get_rect()
+
+
+
+list_wall = [(10, 10),(30, 30), (50,50), (90,90), (120,120), (150, 150)]
+
+continuer = True
+
+while continuer: 
+    ecran.fill(noir) # definit une couleur de fond pour l'ecran
+    for element in list_wall:
+        ecran.blit(picture_floor, (element))
+    for element in list_wall_position:
+        ecran.blit(picture_wall, element)
+    for element in list_floor_position:
+        ecran.blit(picture_floor, element)
+    ecran.blit(picture_floor, list_wall[2]) # blit copie l'image_surface en parametre sur la surface de l'ecran
+    ecran.blit(picture_macgy, (0, 0, 20, 20)) # le tuple 0,0 donne la position de départ de l'image
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuer = False
+    pygame.display.flip() # dit à pygame d'afficher la surface du jeu
+
+pygame.quit()
